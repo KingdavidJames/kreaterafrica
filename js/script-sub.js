0615979f1de89b2802bf8aee4919f8fe52421a68
KingDavid James <71078691+KingdavidJames@users.noreply.github.com>
2023-07-03 02:03:16 +0100

    Add files via upload

--- js/script-sub.js
+++ js/script-sub.js
@@ -36,3 +36,40 @@
     });
   }
 })(window.jQuery);
+
+("use strict");
+const startShipping = document.querySelector(".start-shipping");
+const shipCalculator = document.querySelector(".ship-calculator");
+
+const shipContent = document.getElementById("ship-content");
+const calculatorContent = document.getElementById("ship-calc");
+
+// For Text
+const textStyle = document.querySelector(".start-ship-text");
+const noStyle = document.querySelector(".no-style");
+
+// For Icon
+const iconStyle = document.querySelector(".start-ship-icon");
+const noIconStyle = document.querySelector(".no-icon-style");
+
+startShipping.addEventListener("click", function () {
+  shipContent.classList.remove("hidden");
+  calculatorContent.classList.add("hidden");
+
+  textStyle.classList.add("start-ship-text");
+  noStyle.classList.remove("start-ship-text");
+
+  iconStyle.classList.add("start-ship-icon");
+  noIconStyle.classList.remove("start-ship-icon");
+});
+
+shipCalculator.addEventListener("click", function () {
+  calculatorContent.classList.remove("hidden");
+  shipContent.classList.add("hidden");
+
+  noStyle.classList.add("start-ship-text");
+  textStyle.classList.remove("start-ship-text");
+
+  noIconStyle.classList.add("start-ship-icon");
+  iconStyle.classList.remove("start-ship-icon");
+});
